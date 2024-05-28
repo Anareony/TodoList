03e96cf44718ce159599d0ee5a7441cd1fefe0f6
Chudoyakov Roman <chudoyakov.roman@gmail.com>
2024-05-28 23:50:07 +0700

    feat: created theme config

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,46 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+import { ConfigProvider } from "antd";
+import type { ThemeConfig } from "antd";
+
+const config: ThemeConfig = {
+  token: {
+    colorText: "#fff",
+    colorTextDescription: "#b2b2b2",
+    colorPrimary: "#682ee4",
+  },
+  components: {
+    Checkbox: {
+      controlInteractiveSize: 20,
+      borderRadiusSM: 10,
+      colorBgContainer: "tranparent",
+      lineWidth: 2,
+    },
+    Button: {
+      fontWeight: 600,
+      defaultColor: "#fff",
+      defaultBg: "#682ee4",
+      defaultHoverBg: "#7562fc",
+      defaultHoverColor: "#fff",
+      defaultActiveBg: "#682ee4",
+      defaultActiveColor: "#fff",
+      defaultBorderColor: "transparent",
+    },
+    Typography: {
+      titleMarginBottom: 0,
+      titleMarginTop: 0,
+    },
+    Divider: {
+      marginLG: 0,
+    },
+  },
+};
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ConfigProvider theme={config}>
+      <App />
+    </ConfigProvider>
   </React.StrictMode>
 );
