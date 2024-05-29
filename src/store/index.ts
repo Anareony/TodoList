472d992ddd9b6455b4f9ed83c38fa59a25542d1f
Chudoyakov Roman <chudoyakov.roman@gmail.com>
2024-05-29 23:41:18 +0700

    refactor(store): types, axios and moved favourite todo store to todo store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,106 +1,89 @@
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
-
-interface postTodo {
-  title: string;
-  description: string;
-  status: "completed" | "active";
-}
-
-export interface TodoProps {
-  id: number;
-  attributes: postTodo;
-}
+import { createJSONStorage, persist } from "zustand/middleware";
+import { changeStatusTodo, deleteTodo, getTodos, postTodo } from "../api";
+import { Pagination, Todo, TodoAttributes } from "../types";
 
 interface TodoState {
-  todos: TodoProps[];
+  todos: Todo[];
+  favTodos: Todo[];
+  pagination: Pagination;
+  loading: boolean;
   error: unknown;
-  getTodos: () => void;
-  postTodo: (todo: postTodo) => void;
-  changeStatusTodo: (id: number, status: postTodo["status"]) => void;
-  deleteTodo: (id: number) => void;
+  getTodos: (page: number) => void;
+  postTodo: (todo: TodoAttributes) => void;
+  changeStatusTodo: (
+    id: number,
+    { status }: { status: TodoAttributes["status"] }
+  ) => void;
+  deleteTodo: (todoId: Todo["id"]) => void;
+  setFavTodos: (todo: Todo) => void;
+  filterFavTodo: (todoId: Todo["id"]) => void;
+  isFav: (todoId: Todo["id"]) => boolean;
 }
 
-const accessToken =
-  "a56017bfd8f1a9d1c8d012e881ef7df90ddc4e3d74e61a27b82fa975cfe37571fcb0e7617258e871291c4315b68c1c410274fb19269becf5dae7b5372d611d66c605c701817bd70f8fcd39aa44973e95fb1dff1b36e3271ba4bf890e074e52d9b9feddcee0947e588d7b5f6eef4bd4ead3993c6ee7b35ffddf22012c2b5589ed";
-
-const BASE_URL = "https://cms.dev-land.host";
-
-export const useTodosStore = create<TodoState>((set) => ({
-  todos: [],
-  error: null,
-  getTodos: async () => {
-    try {
-      const response = await fetch(`${BASE_URL}/api/tasks`, {
-        method: "GET",
-        headers: {
-          Authorization: `bearer  + ${accessToken}`,
-        },
-      }).then((res) => res.json());
-
-      set({ todos: response.data });
-    } catch (error: unknown) {
-      set({ error: error });
-    } finally {
-      console.log("work");
+export const useTodosStore = create<TodoState>()(
+  persist(
+    (set, get) => ({
+      todos: [],
+      favTodos: [],
+      pagination: {} as Pagination,
+      error: null,
+      loading: false,
+      getTodos: async (page) => {
+        set({ loading: true });
+        try {
+          const response = await getTodos(page);
+          if (response.status === 200) {
+            const body = await response.data;
+            set({ todos: [...get().todos, ...body.data] });
+            set({ pagination: body.meta.pagination });
+          }
+        } catch (error: unknown) {
+          set({ error: error });
+        } finally {
+          set({ loading: false });
+        }
+      },
+      postTodo: async (data) => {
+        const response = await postTodo(data);
+        const body = await response.data;
+        console.log(body);
+        // set({ todos: [response.data, ...get().todos] });
+      },
+      changeStatusTodo: async (id, data) => {
+        const response = await changeStatusTodo(id, data);
+        // const body = await response.data;
+      },
+      deleteTodo: async (id) => {
+        const response = await deleteTodo(id);
+        if (response.status === 200) {
+          set({ todos: get().todos.filter((todoId) => todoId.id !== id) });
+          set({
+            favTodos: get().favTodos.filter((todoId) => todoId.id !== id),
+          });
+        }
+      },
+      setFavTodos: (todo) => set({ favTodos: [...get().favTodos, todo] }),
+      filterFavTodo: (id) =>
+        set({ favTodos: get().favTodos.filter((todoId) => todoId.id !== id) }),
+      isFav: (id) => get().favTodos.some((todo) => todo.id === id),
+    }),
+    {
+      name: "favTodos",
+      partialize: (state) => ({ favTodos: state.favTodos }),
+      storage: createJSONStorage(() => localStorage),
     }
-  },
-  postTodo: async (data) => {
-    const response = await fetch(`${BASE_URL}/api/tasks`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `bearer  + ${accessToken}`,
-      },
-      body: JSON.stringify({ data }),
-    });
-  },
-  changeStatusTodo: async (id, data) => {
-    const response = await fetch(`${BASE_URL}/api/tasks/${id}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `bearer  + ${accessToken}`,
-      },
-      body: JSON.stringify({ data }),
-    });
-  },
-  deleteTodo: async (id) => {
-    const response = await fetch(`${BASE_URL}/api/tasks/${id}`, {
-      method: "DELETE",
-      headers: {
-        Authorization: `bearer  + ${accessToken}`,
-      },
-    });
-  },
-}));
+  )
+);
+
+type Filter = "all" | "active" | "completed" | "favourite";
 
 interface Filters {
-  filter: "all" | "active" | "completed";
-  setFilter: (value: "all" | "active" | "completed") => void;
+  filter: Filter;
+  setFilter: (value: Filter) => void;
 }
 
 export const useFilter = create<Filters>((set) => ({
   filter: "all",
   setFilter: (value) => set({ filter: value }),
 }));
-
-interface TodoFavState {
-  favTodos: TodoProps[];
-  setFavTodos: (todo: TodoProps) => void;
-  filterFavTodo: (todoId: TodoProps["id"]) => void;
-  isFav: (todoId: TodoProps["id"]) => boolean;
-}
-
-export const useFavTodosStore = create<TodoFavState>()(
-  persist(
-    (set, get) => ({
-      favTodos: [],
-      setFavTodos: (todo) => set({ favTodos: [...get().favTodos, todo] }),
-      filterFavTodo: (id) =>
-        set({ favTodos: get().favTodos.filter((todoId) => todoId.id !== id) }),
-      isFav: (id) => get().favTodos.some((todo) => todo.id === id),
-    }),
-    { name: "favTodos" }
-  )
-);
