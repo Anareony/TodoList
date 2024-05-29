e1f2cb4e1ad395ad34cddd10405e7b6b572a007b
Chudoyakov Roman <chudoyakov.roman@gmail.com>
2024-05-29 23:44:15 +0700

    feat :import todolist and buttons to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
-import "./App.css";
+import { Layout as AntdLayout } from "antd";
+
+import { CreateTodoModal } from "./components/CreateTodoModal";
+import { FilterButtons } from "./components/FilterButtons";
+import { TodoList } from "./components/TodoList";
+import styled from "styled-components";
+
+const { Content, Sider: AntdSider } = AntdLayout;
+
+const Layout = styled(AntdLayout)`
+  height: 100vh;
+  overflow: auto;
+  background: #111214;
+  padding: 20px;
+`;
+
+const Sider = styled(AntdSider)`
+  height: fit-content;
+  border-radius: 10px;
+  background: transparent !important;
+  margin-right: 20px;
+  position: sticky !important;
+  top: 0;
+`;
 
 function App() {
-  return <></>;
+  return (
+    <Layout>
+      <Sider>
+        <FilterButtons />
+        <CreateTodoModal />
+      </Sider>
+      <Content>
+        <TodoList />
+      </Content>
+    </Layout>
+  );
 }
 
 export default App;
