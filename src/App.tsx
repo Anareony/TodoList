aeedf8f4ea9f92fea9218c9e463b8680b860b3e1
Chudoyakov Roman <chudoyakov.roman@gmail.com>
2024-05-31 00:00:04 +0700

    feat: media and import notification

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
+import styled from "styled-components";
+
 import { Layout as AntdLayout } from "antd";
 
 import { CreateTodoModal } from "./components/CreateTodoModal";
 import { FilterButtons } from "./components/FilterButtons";
 import { TodoList } from "./components/TodoList";
-import styled from "styled-components";
+import { Notification } from "./components/Notification";
 
-const { Content, Sider: AntdSider } = AntdLayout;
+const { Content: AntdContent, Sider: AntdSider } = AntdLayout;
 
 const Layout = styled(AntdLayout)`
   height: 100vh;
-  overflow: auto;
+  overflow-y: auto;
   background: #111214;
   padding: 20px;
+  @media (max-width: 768px) {
+    flex-direction: column !important;
+    justify-content: center !important;
+    align-items: center;
+    padding: 10px;
+  }
 `;
 
 const Sider = styled(AntdSider)`
@@ -21,6 +29,18 @@ const Sider = styled(AntdSider)`
   margin-right: 20px;
   position: sticky !important;
   top: 0;
+  @media (max-width: 768px) {
+    position: relative !important;
+    min-width: 100% !important;
+    flex: unset !important;
+    margin: 10px;
+  }
+`;
+
+const Content = styled(AntdContent)`
+  @media (max-width: 768px) {
+    width: 100% !important;
+  }
 `;
 
 function App() {
@@ -33,6 +53,7 @@ function App() {
       <Content>
         <TodoList />
       </Content>
+      <Notification />
     </Layout>
   );
 }
