74766060319275ff59089866188dc8fd2db9cb38
Chudoyakov Roman <chudoyakov.roman@gmail.com>
2024-05-30 23:59:03 +0700

    feat: added pop confirm for delete button

--- src/components/DeleteTodoButton/index.tsx
+++ src/components/DeleteTodoButton/index.tsx
@@ -1,18 +1,32 @@
-import { Button } from "antd";
-import { useTodosStore } from "../../store";
+import styled from "styled-components";
+
+import { Button as AntdButton, Popconfirm, PopconfirmProps } from "antd";
 import { DeleteFilled } from "@ant-design/icons";
+
+import { useTodosStore } from "../../store";
+
+const Button = styled(AntdButton)`
+  min-width: 32px;
+`;
 
 export const DeleteTodoButton = ({ id }: { id: number }) => {
   const { deleteTodo } = useTodosStore((state) => ({
     deleteTodo: state.deleteTodo,
   }));
 
+  const confirm: PopconfirmProps["onConfirm"] = () => {
+    deleteTodo(id);
+  };
+
   return (
-    <Button
-      type="primary"
-      danger
-      onClick={() => deleteTodo(id)}
-      icon={<DeleteFilled />}
-    />
+    <Popconfirm
+      title="Delete the task"
+      description="Are you sure to delete this task?"
+      onConfirm={confirm}
+      okText="Yes"
+      cancelText="No"
+    >
+      <Button type="primary" danger icon={<DeleteFilled />} />
+    </Popconfirm>
   );
 };
